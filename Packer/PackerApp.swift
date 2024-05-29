1d250b6cf38a50ca1d4ba7f937e6b6295cbdb681
Sophie Haber <SHaber@lcs.on.ca>
2024-05-29 09:48:04 -0400

    Initial Commit

---
+++ Packer/PackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PackerApp.swift
+//  Packer
+//
+//  Created by Sophie Haber on 29.05.24.
+//
+
+import SwiftUI
+
+@main
+struct PackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
