b1da1ead951a900aef5e97ec6862049b39eef0bb
Sophie Haber <SHaber@lcs.on.ca>
2024-06-06 12:08:06 -0400

    I created the splash screen

---
+++ Packer/SplashScreenView.swift
@@ -0,0 +1,70 @@
+//
+//  SplashScreenView.swift
+//  Packer
+//
+//  Created by Sophie Haber on 06.06.24.
+//
+
+import SwiftUI
+
+struct SplashScreenView: View {
+    var body: some View {
+        ZStack{
+            
+            //first layer
+            Color.orangePink
+                .ignoresSafeArea()
+            
+            VStack{
+                //second layer
+                HStack{
+                    
+                    Spacer()
+                    
+                    Text ("Packer")
+                        .font(.system(size: 60))
+                        .bold()
+                        .foregroundColor(.white)
+                    
+                    Spacer()
+                    Spacer()
+                    
+                }.padding()
+               
+                HStack{
+                    
+                    Spacer()
+                    Spacer()
+                    
+                    Text ("Stay organized")
+                        .bold()
+                        .foregroundColor(.white)
+                    
+                    Spacer()
+                    
+                }.padding(.horizontal)
+                
+                Spacer()
+                
+                Image(systemName: "checkmark.circle.fill")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 70)
+                    .foregroundColor(.logoPurple)
+                
+                Spacer()
+                
+                Text ("Get Started")
+                    .bold()
+                    .foregroundColor(.white)
+                    .font(.largeTitle)
+                Spacer()
+            }
+            
+        }
+    }
+}
+
+#Preview {
+    SplashScreenView()
+}
