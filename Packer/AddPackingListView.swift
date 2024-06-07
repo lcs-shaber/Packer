46e6f1bdefad973c6d4e0dc3e85f016f02f56327
Sophie Haber <SHaber@lcs.on.ca>
2024-06-07 14:55:10 -0400

    I created a sheet which slides up when you press on the add button

--- Packer/AddPackingListView.swift
+++ Packer/AddPackingListView.swift
@@ -13,57 +13,78 @@ struct AddPackingListView: View {
     
     let day: Calendar
     
+    // Is the sheet to add a new to-do item showing right now?
+    @State var presentingNewItemSheet = false
+    
     // MARK: Computed properties
     
     var body: some View {
         
-        ZStack {
+        NavigationView {
             
-            // Background Color
-            Color("OrangePink")
-                .ignoresSafeArea()
-            
-            VStack {
+            ZStack {
                 
-                HStack {
-                    
-                    Spacer()
-                    
-                    Image (systemName: "plus")
-                        .bold()
-                        .foregroundColor(.darkBlue)
-                        .font(.system(size: 30))
-                        .padding(.horizontal)
-                }
+                // Background Color
+                Color("OrangePink")
+                    .ignoresSafeArea()
                 
-                Text("What do you need to bring?")
-                    .bold()
-                    .font(.title)
-                    .foregroundColor(Color.darkGray)
-                    .padding(.bottom)
-                
-                ZStack {
-                    RoundedRectangle(cornerRadius: 30 )
-                        .fill(Color.darkBlue)
-                        .frame(height: 70)
-                        .padding(.horizontal)
+                VStack {
                     
-                    Text (day.reminders)
-                        .foregroundColor(.white)
+                    Text("What do you need to bring?")
                         .bold()
                         .font(.title)
-                }
-                
-                List {
-                   
-                    Text (day.reminders2)
+                        .foregroundColor(Color.darkGray)
+                        .padding(.bottom)
+                    
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 30 )
+                            .fill(Color.darkBlue)
+                            .frame(height: 70)
+                            .padding(.horizontal)
+                        
+                        Text (day.reminders)
+                            .foregroundColor(.white)
+                            .bold()
+                            .font(.title)
+                    }
+                    
+                    List {
+                        
+                        Text (day.reminders2)
+                        
+                    }
+                    .listStyle(.inset)
+                    .padding(.horizontal)
+                    .padding(.bottom)
                     
                 }
-                .listStyle(.inset)
-                .padding(.horizontal)
-                .padding(.bottom)
-                
             }
+            
+            //show the sheet to a new Item
+            .sheet(
+                isPresented: $presentingNewItemSheet
+            ) {
+                Text("Hello, world!")
+                    .presentationDetents([.medium, .fraction(0.15)])
+            }
+            
+            // Add a tool bar to the top of the interface
+            // NOTE: For a toolbar to appear, it must be
+            //       inside a NavigationView or NavigationStack.
+            .toolbar {
+                // Add a button to trigger showing the sheet
+                ToolbarItem(placement: .automatic) {
+                    Button {
+                        presentingNewItemSheet = true
+                    } label: {
+                        Image(systemName: "plus")
+                            .bold()
+                            .font(.title2)
+                            .foregroundColor(.darkBlue)
+                    }
+                }
+            }
+            
         }
     }
 }
